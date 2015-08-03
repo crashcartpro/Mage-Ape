c636ae0d536223e7d3692d6adc90b0635d6a1b5c
crashcartpro <crashcartpro@hotmail.com>
2015-08-03 15:02:50 -0500

    Form retains last entry after input

--- README.md
+++ README.md
@@ -15,3 +15,8 @@ Mage Ape is a generic web interface for testing API calls to Magento stores and 
 * ~~add user/pass feilds~~
 * ~~make authed function calls in "step 2"~~
 * ~~trap and report errors for troubleshooting~~
+* check that url actually resolves before doing anything else.
+* keep a more detailed log of calls and responces, and print it out after all tests complete.
+* optional switch for Magento SOAP v1 or SOAP v2
+* more optional switches for XML-RPC?
+* ~~have the form retain previous values after submiting~~
--- index.php
+++ index.php
@@ -9,11 +9,18 @@
 
 ob_implicit_flush(1);
 
-#defaults for testing
-$inputurl = "www.theath.simple-helix.net";
-$user = "theath";
-$pass = "donttell";
-
+if (!empty($_POST)) {
+  # Fetch POST variables
+  $inputurl = $_POST['website'];
+  $user = $_POST['user'];
+  $pass = $_POST['pass'];
+  #$apimehod = $_POST['apimethod'];
+} else {  
+  #defaults for testing
+  $inputurl = "www.theath.simple-helix.net";
+  $user = "theath";
+  $pass = "donttell";
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -26,7 +33,7 @@ $pass = "donttell";
   <div class="pull-right">
     <img src="Mage_ape1.png">
   </div>
-  <div class="col-sm-9 col-md-6 col-md-offset-1">
+  <div class="col-sm-11 col-md-6 col-md-offset-2">
     <h1><a href="http://taoexmachina.com/mage-ape">Mage Ape</a><small>  Magento API test</small></h1>
     <div class="row">
       Try: http://www.theath.simple-helix.net/index.php/api/v2_soap/?wsdl
@@ -36,44 +43,44 @@ $pass = "donttell";
         <div class="form-group">
           <div class="input-group">
             <div class="input-group-addon">Site:</div>
-            <input type="text" class="form-control" name="website" value="www.theath.simple-helix.net">
+            <input type="text" class="form-control" name="website" value="<?php echo $inputurl; ?>">
             <span class="input-group-btn">
               <button type="submit" class="btn btn-primary">Get Info</button>
             </span>
           </div>
         </div>
         <div class="input-group">
-            <div class="input-group-addon">Username:</div>
-            <input type="text" class="form-control" name="user" value="theath">
-            <div class="input-group-addon">Password:</div>
-            <input type="text" class="form-control" name="pass" value="donttell">
-            <!--<span class="input-group-btn">
-              <button type="submit" class="btn btn-primary">Run Tests</button>
-            </span>-->
-          </div>
-      </form>
-    </div>
+          <div class="input-group-addon">Username:</div>
+          <input type="text" class="form-control" name="user" value="<?php echo $user;?>">
+          <div class="input-group-addon">Password:</div>
+          <input type="password" class="form-control" name="pass" value="<?php echo $pass; ?>">
+          <!--<span class="input-group-btn">
+            <button type="submit" class="btn btn-primary">Run Tests</button>
+          </span>
+        </div>
+        <div class="input-group">
+            <div class="input-group-addon">command:</div>
+            <input type="text" class="form-control" name="apimethod" value="storeList">-->
+        </div>
 <?php
 if (!empty($_POST)) {
   #loading gif here and visable
   ob_flush();
 
-  # Fetch POST variables
-  $inputurl = $_POST['website'];
-  $user = $_POST['user'];
-  $pass = $_POST['pass'];
-  
   # Filter URL. 
   # If http is missing, add http, 
   # If wsdl isn't declared, add wsdl and path
   $url = filter_var($inputurl, FILTER_SANITIZE_URL);
   if (strpos($url, "http://") !== 0) {$url = "http://" . $url;}
+  $urlparts = parse_url($url);
   if (strpos($url, "wsdl") !== (strlen($url)-4)) {
-    $urlparts = parse_url($url);
     $url = "http://" . $urlparts["host"] . "/index.php/api/v2_soap/?wsdl";    
   }
+#  $headers = @get_headers($url);
+#  echo "<div class='alert alert-warning' role='alert'>". var_dump($headers). "</div>";
   echo '<div class="alert alert-info" role="alert">Using: ' . $url . "</div>";
   ob_flush();
+   
 
   # catch errors
   try {
