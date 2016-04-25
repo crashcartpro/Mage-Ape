69b872f3ee6acae738420a62823ce9fae489b512
crashcartpro <crashcartpro@hotmail.com>
2016-04-25 14:32:14 -0500

    Switched to bootstrap 4.0 alpha fussed around a bit to get hopefully better responsiveness with the layout.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 #########################
 #  Mage-Ape
-#    v0.8 
+#    v0.8.1 
 # by CrashCart
 #
 # Mage-Ape is an atempt at a tool for testing and diagnosing errors with Magento API calls
@@ -23,13 +23,13 @@ function correctURL($inputurl) {
 	return $workingurl;
 }
 
-function postMessage($type, $title, $message) {
+function postMessage($type, $title, $message = "none") {
 	global $starttime;
 	$timestamp = round(microtime(true)-$starttime,2);
 	echo '<div class="alert '.$type.'" role="alert">';
 	echo '<div style="float:right">'.$timestamp.'</div>';
 	echo '<h4>'.$title.'</h4>';
-	if (!empty($message)) {echo $message;}
+	if ($message != "none") {echo $message;}
 	echo "</div>\r\n";
 }
 
@@ -50,53 +50,56 @@ if (!empty($_POST)) {
 <html>
 <head>
 	<title>Mage Ape</title>
-	<link rel="stylesheet" href="includes/bootstrap.min.css">
+	<!--<link rel="stylesheet" href="includes/bootstrap_3.3.5_min.css">-->
+	<link rel="stylesheet" href="includes/bootstrap_4.0.0_alpha2.css">
 	<script src"includes/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="container">
-	<div class="pull-right">
-		<img src="Mage_ape1.png"><br>
-
-	</div>
-	<div class="col-sx-12 col-sm-6 col-md-6 col-md-offset-2">
-		<h1><a href="http://taoexmachina.com/mage-ape">Mage Ape</a><small>  Magento API test</small></h1>
-		<p>Magento is a highly extensable e-ecomerce framework with many moving parts. Just one such part is the SOAP or XML-RPC based API interface. Which allows 3rd party programs to access store content.</p>
-		<p>However, sometimes things fail. Mage Ape wants to help you troubleshoot.</p>
-		<p>Start by entering your domain. <code>example.com</code> Mage Ape will assume defaults and test unauthenticated requests. You can also specify the full path to the WSDL. <code>www.example.com/index.php/api/v2_soap/?wsdl</code></p>
-		<p>If you specify the user and password, Mage Ape will start a session and try to pull some data from the store. If any step fails, Mage Ape dutifully prints the error message for you you see.</p>
-		<div class="row">Try: http://www.theath.simple-helix.net/index.php/api/v2_soap/?wsdl</div>
-		<div class-"row">
-			<form action="" method="POST">
-				<div class="form-group">
-					<div class="input-group">
-						<div class="input-group-addon">URL:</div>
-						<input type="text" class="form-control" name="website" value="<?php echo $inputurl; ?>">
-						<span class="input-group-btn">
-							<button type="submit" class="btn btn-primary">Get Info</button>
-						</span>
-					</div>
-					<div class="input-group">
-						<div class="input-group-addon">Username:</div>
-						<input type="text" class="form-control" name="user" value="<?php echo $user;?>">
-						<div class="input-group-addon">Password:</div>
-						<input type="password" class="form-control" name="pass" value="<?php echo $pass; ?>">
-					</div>
-				</div>
-			</form>
+	<div class="row">
+		<div class="col-xs-12 col-md-4 text-xs-center text-md-left pull-md-right">
+			<img src="Mage_ape1.png" style="width:100%;max-width:220px;"><br>
 		</div>
-		<div class-"row">
-
+		<div class="col-sx-12 col-md-7 offset-lg-1">
+			<h1><a href="http://taoexmachina.com/mage-ape">Mage Ape</a><small> Magento&nbspAPI&nbsptest</small></h1>
+			<p>Magento is a highly extensable e-ecomerce framework with many moving parts. Just one such part is the SOAP or XML-RPC based API interface. Which allows 3rd party programs to access store content.</p>
+			<p>However, sometimes things fail. Mage Ape wants to help you troubleshoot.</p>
+			<p>Start by entering your domain. <code>example.com</code> Mage Ape will assume defaults and test unauthenticated requests. You can also specify the full path to the WSDL. <code>www.example.com/index.php/api/v2_soap/?wsdl</code></p>
+			<p>If you specify the user and password, Mage Ape will start a session and try to pull some data from the store. If any step fails, Mage Ape dutifully prints the error message for you you see.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-lg-8 offset-lg-2">
+			<p>Try: http://www.theath.simple-helix.net/index.php/api/v2_soap/?wsdl</p>
+				<form action="" method="POST">
+					<div class="form-group">
+						<div class="input-group">
+							<div class="input-group-addon">URL:</div>
+							<input type="text" class="form-control" name="website" value="<?php echo $inputurl; ?>">
+							<span class="input-group-btn">
+								<button type="submit" class="btn btn-primary">Get Info</button>
+							</span>
+						</div>
+						<div class="input-group">
+							<div class="input-group-addon">Username:</div>
+							<input type="text" class="form-control" name="user" value="<?php echo $user;?>">
+							<div class="input-group-addon">Password:</div>
+							<input type="password" class="form-control" name="pass" value="<?php echo $pass; ?>">
+						</div>
+					</div>
+				</form>
+			<p>
 <?php
 
 
 if (!empty($_POST)) {
-	## loading gif here? and visable
+	## loading gif 
 	echo '<img id="loadingGif" style="position:absolute;bottom:-20px;left:46%;" src="ape-loader2.gif">';
 	ob_flush();
 	$starttime = microtime(true);
 ## Filter URL. 
 	$url = correctURL($inputurl);
+	#$url = $inputurl;
 	postMessage("alert-info", "Started test using:", $url);
 	ob_flush();
 
@@ -113,9 +116,8 @@ if (!empty($_POST)) {
 	}
 	ob_flush(); 
 
-	# catch errors
+## Create soap conneciton and run tests
 	try {
-		# setup SOAP client connection and login
 		$options = array('exceptions'=>true, 'trace'=>1);
 		$client = new SoapClient($url, $options);
 		if (empty($user) || empty($pass)) {
@@ -156,13 +158,11 @@ if (!empty($_POST)) {
 
 }
 ?>
+			</p>
 		</div>
 	</div>
 </div>
 
-<script>
-document.getElementById('loadingGif').style.display = 'none';
-</script>
-
+<script>document.getElementById('loadingGif').style.display = 'none';</script>
 </body></html>
 
